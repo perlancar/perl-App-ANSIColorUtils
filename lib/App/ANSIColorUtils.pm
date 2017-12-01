6e8f5efff98a6913d458ebd6192f7c033b3fd401
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-12-02 05:08:55 +0700

    Initial commit

---
+++ lib/App/ANSIColorUtils.pm
@@ -0,0 +1,26 @@
+package App::ANSIColorUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+$SPEC{show_ansi_code}
+
+1;
+#ABSTRACT: Utilities related to ANSI color
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
